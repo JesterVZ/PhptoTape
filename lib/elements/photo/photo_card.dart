03642fdd37aebfa0812cf7775b304735a646ee73
Vladimir <justjestervz@gmail.com>
2022-05-30 19:30:17 +0500

    show photo

--- lib/elements/photo/photo_card.dart
+++ lib/elements/photo/photo_card.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class PhotoCard extends StatefulWidget{
+class PhotoCard extends StatefulWidget {
+  String url;
+  PhotoCard({required this.url});
   @override
   State<StatefulWidget> createState() => _PhotoCard();
 }
 
-class _PhotoCard extends State<PhotoCard>{
+class _PhotoCard extends State<PhotoCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 300,
+      height: 300,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 5,
+          blurRadius: 7,
+          offset: Offset(0, 3),
+        ),
+        
+      ]),
+      child: Image(image: NetworkImage(widget.url)),
+    );
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_tape/bloc/main_bloc.dart';
 import 'package:photo_tape/bloc/main_state.dart';
+import 'package:photo_tape/elements/photo/photo_card.dart';
+import 'package:photo_tape/model/photo_model.dart';
 
 import '../DI/dependency-provider.dart';
 import '../elements/bloc/bloc_screen.dart';
@@ -15,26 +17,32 @@ class MainPage extends StatefulWidget{
 class _MainPage extends State<MainPage>{
   MainBloc? mainBloc;
   int page = 1;
+  ScrollController scrollController = ScrollController();
+  List<PhotoModel> photos = [];
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return BlocScreen<MainBloc, MainState>(
-      bloc: mainBloc,
+      bloc: mainBloc, 
       listener: (context, state) => _listener(context, state),
       builder: (context, state) {
         return Scaffold(
           body:  Stack(
             children: [
-
+              Scrollbar(
+                child: ListView.builder(itemCount: photos.length, controller: scrollController, reverse: true, itemBuilder: (BuildContext context, int i){
+                  return PhotoCard(url: photos[i].url!);
+                })
+              ),
               Visibility(
+                      visible: isLoading,
                       child: Center(
-                          child: Container(
+                          child: SizedBox(
                         width: 50,
                         height: 50,
                         child: Image.asset('assets/loading.gif'),
-                      )),
-                      visible: isLoading)
+                      )))
             ],
           )
         );
@@ -47,7 +55,7 @@ class _MainPage extends State<MainPage>{
       return;
     }
     if(state.photos != null){
-      
+      photos = state.photos!;
     }
   }
   @override
