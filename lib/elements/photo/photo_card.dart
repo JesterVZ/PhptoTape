4a2a1f58c4074fa39552d687bd2e47df4689801b
Jester <justjestervz@gmail.com>
2022-06-01 02:50:28 +0300

    like button

--- lib/elements/photo/photo_card.dart
+++ lib/elements/photo/photo_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../model/photo_model.dart';
 
@@ -20,21 +21,36 @@ class _PhotoCard extends State<PhotoCard> {
         },
         child: Padding(
             padding: EdgeInsets.all(20),
-            child: Container(
-              width: 300,
-              height: 300,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ]),
-              child: Image(image: NetworkImage(widget.photo.url!)),
+            child: Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ]),
+                  child: Image(image: NetworkImage(widget.photo.url!)),
+                ),
+                Positioned(
+                    bottom: 5,
+                    left: 5,
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      child: SvgPicture.asset(
+                        'assets/like.svg',
+                        color: Colors.grey,
+                      ),
+                    ))
+              ],
             )));
   }
 }
